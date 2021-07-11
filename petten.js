b6eb2bece689c561a12b42d6ee5ca2c2c75cbad2
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-07-11 12:53:26 +0530

    Add files via upload

---
+++ petten.js
@@ -0,0 +1,17 @@
+var i=1
+var a=""
+var b="* "
+while (i<=4){
+    a=a+b
+    console.log(a)
+    i=i+1
+}
+// var j=3
+// var c=""
+// var d ="* * *"
+// while (j>=1){
+//     c=c+d
+//     console.log(c)
+//     j=j-1
+// }
+
